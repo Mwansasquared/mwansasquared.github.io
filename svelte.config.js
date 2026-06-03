952a8a378add9be67b11bf2fb684ae75145ca080
Mwansa Mwansa <49552426+Mwansasquared@users.noreply.github.com>
2026-06-03 22:59:10 +0200

    fix failing blank page on url

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/kit/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/mwansa-portfolio'
+			base: process.env.BASE_PATH ?? ''
 		}
 	}
 };
